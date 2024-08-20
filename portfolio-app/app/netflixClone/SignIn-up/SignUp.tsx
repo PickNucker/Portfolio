e932001900feff5e067ddb4b4d710bfe8a79143f
PickNucker <68569018+PickNucker@users.noreply.github.com>
2024-08-20 12:52:13 +0200

    Profile Page

--- portfolio-app/app/netflixClone/SignIn-up/SignUp.tsx
+++ portfolio-app/app/netflixClone/SignIn-up/SignUp.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useCallback, useState } from 'react';
 import {
   Card,
   Input,
@@ -9,34 +10,64 @@ import {
 import { TermsOfService } from './TermsOfService';
 
 export function SignUp() {
+  const [renderLogin, setRenderLogin] = useState('login');
+
+  const toggleRender = useCallback(() => {
+    setRenderLogin((currentRender) =>
+      currentRender === 'login' ? 'register' : 'login'
+    );
+  }, []);
+
   return (
     <Card color="transparent" shadow={false}>
       <Typography variant="h2" color="blue-gray" className="text-gray-300">
-        Sign Up
+        {renderLogin === 'login' ? 'Login' : 'Register'}
       </Typography>
       <Typography
         color="gray"
-        className="mt-1 font-normal text-gray-600 text-xl"
+        className="mt-1 font-normal text-gray-600 text-xl w-96"
       >
-        Nice to meet you! Enter your details to register.
+        {renderLogin === 'login'
+          ? 'Welcome back'
+          : 'Nice to meet you! Enter your details to register.'}
       </Typography>
-      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-auto">
+      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
         <div className="mb-1 flex flex-col gap-6">
-          <Typography
-            variant="h6"
-            color="blue-gray"
-            className="-mb-3 text-gray-400"
-          >
-            Your Name
-          </Typography>
-          <Input
-            size="lg"
-            placeholder="JohnDoe"
-            className=" !border-t-blue-gray-200 focus:!border-t-gray-900 text-gray-300"
-            labelProps={{
-              className: 'before:content-none after:content-none'
-            }}
-          />
+          {renderLogin === 'register' && (
+            <div>
+              <Typography
+                variant="h6"
+                color="blue-gray"
+                className="mb-3 text-gray-400"
+              >
+                First Name
+              </Typography>
+              <Input
+                size="lg"
+                placeholder="John"
+                className=" !border-t-blue-gray-200 focus:!border-t-gray-900 text-gray-300"
+                labelProps={{
+                  className: 'before:content-none after:content-none'
+                }}
+              />
+
+              <Typography
+                variant="h6"
+                color="blue-gray"
+                className="mb-3 mt-3 text-gray-400"
+              >
+                Last Name
+              </Typography>
+              <Input
+                size="lg"
+                placeholder="Doe"
+                className=" !border-t-blue-gray-200 focus:!border-t-gray-900 text-gray-300"
+                labelProps={{
+                  className: 'before:content-none after:content-none'
+                }}
+              />
+            </div>
+          )}
           <Typography
             variant="h6"
             color="blue-gray"
@@ -69,31 +100,39 @@ export function SignUp() {
             }}
           />
         </div>
-        <Checkbox
-          label={
-            <Typography
-              variant="small"
-              color="gray"
-              className="flex items-center font-normal"
-            >
-              I agree the
-              <div>
-                <TermsOfService />
-              </div>
-            </Typography>
-          }
-          containerProps={{ className: '-ml-2.5' }}
-        />
+        {renderLogin === 'register' && (
+          <Checkbox
+            label={
+              <Typography
+                variant="small"
+                color="gray"
+                className="flex items-center font-normal"
+              >
+                I agree the
+                <div>
+                  <TermsOfService />
+                </div>
+              </Typography>
+            }
+            containerProps={{ className: '-ml-2.5' }}
+          />
+        )}
         <Button className="mt-6 shadow-none" fullWidth color="red">
-          sign up
+          {renderLogin === 'login' ? 'Login' : 'Register'}
         </Button>
         <Typography
           color="gray"
           className="mt-4 text-center font-normal text-gray-600"
         >
-          Already have an account?{' '}
-          <a href="#" className="font-medium hover:text-red-500 text-gray-200">
-            Sign In
+          {renderLogin === 'login'
+            ? ' New on Netflix?  '
+            : ' You already have an Account?  '}
+          <a
+            href="#"
+            className="font-medium hover:text-red-500 text-gray-200"
+            onClick={toggleRender}
+          >
+            {renderLogin === 'login' ? 'SignIn' : 'SignUp'}
           </a>
         </Typography>
       </form>
--- portfolio-app/app/netflixClone/netflixCloneApp.tsx
+++ portfolio-app/app/netflixClone/netflixCloneApp.tsx
@@ -4,13 +4,17 @@ import { SignUp } from './SignIn-up/SignUp';
 
 const NetflixCloneApp = () => {
   return (
-    <div className="relative  min-h-screen w-full bg-[url('/netflix_bg.jpg')] bg-no-repeat bg-fixed bg-center bg-cover">
+    <div className="relative  min-h-screen w-full bg-[url('/hero.jpg')] bg-no-repeat bg-fixed bg-center bg-cover">
       <div className="bg-black min-h-screen w-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
-          <img src="/netflix_logo.png" alt="netflix-logo" className="h-52" />
+          <img
+            src="/logo.png"
+            alt="netflix-logo"
+            className="h-20 mt-12 mb-20"
+          />
         </nav>
         <div className="flex justify-center">
-          <div className="bg-black bg-opacity-75 py-3 px-5 border-4 rounded-lg border-black">
+          <div className="bg-black bg-opacity-75 py-7 px-12 border-4 rounded-lg border-black">
             <SignUp />
           </div>
         </div>
